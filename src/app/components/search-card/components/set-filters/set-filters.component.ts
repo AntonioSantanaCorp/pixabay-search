6a09a2a24fb22275e55943af95c20c0e46259181
AntonioSantanaCorp <antoniocorporativo021@gmail.com>
2023-07-31 22:43:26 -0500

    set filter

--- src/app/components/search-card/components/set-filters/set-filters.component.ts
+++ src/app/components/search-card/components/set-filters/set-filters.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, inject } from '@angular/core';
+import { Component, OnDestroy, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
@@ -6,7 +6,7 @@ import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatButtonModule } from '@angular/material/button';
-import { MatDialogModule } from '@angular/material/dialog';
+import { MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 import { MatDividerModule } from '@angular/material/divider';
 import { FormArray, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { FILTER_FORM } from 'src/app/core/constant/main.constant';
@@ -32,18 +32,23 @@ import { FiltersConfigService } from 'src/app/services/filters-config.service';
   templateUrl: './set-filters.component.html',
   styleUrls: ['./set-filters.component.scss']
 })
-export class SetFiltersComponent implements OnDestroy {
+export class SetFiltersComponent implements OnInit, OnDestroy {
+  private readonly _filtersService = inject(FiltersConfigService)
+
+  private readonly _dialogRef = inject(MatDialogRef<SetFiltersComponent>)
 
   protected readonly filterForm = new FormGroup(FILTER_FORM)
 
-  private readonly _filtersService = inject(FiltersConfigService)
+  ngOnInit(): void {
+    this.filterForm.patchValue(this._filtersService.filterObject())
+  }
 
   ngOnDestroy(): void {
     this.filterForm.reset()
   }
 
   saveChanges() {
-    console.log(this.filterForm.value)
     this._filtersService.saveFilters(this.filterForm.value)
+    this._dialogRef.close()
   }
 }
